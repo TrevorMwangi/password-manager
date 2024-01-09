c76dc55e19b7a2abd9c14593d162017e6df4a9c2
Trevor <82081705+TrevorMwangi@users.noreply.github.com>
2024-01-09 23:30:52 +0300

    commits

--- pass_manager.py
+++ pass_manager.py
@@ -1,28 +1,38 @@
 from cryptography.fernet import Fernet
 
-# master password here
-master_password = input("What is the master password?  ")
 
+'''
 def write_key():
     key = Fernet.generate_key()
     with open("key.key", "wb") as key_file:
-        key_file.write(key)
+        key_file.write(key) '''
 
-write_key()
+
+def load_key():
+    file =  open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+
+master_password = input("What is the master password?  ")
+key = load_key() + master_password.encode()
+fer = Fernet(key)
+
 
 def view():
     with open("passwords.txt", "r") as f:
         for line in f.readlines():
             data = line.rstrip()
             user, passw = data.split("|")
-            print("Username: ", user, "| Password: ", passw)
+            print("Username: ", user, "| Password: ", fer.decrypt(passw.encode()).decode())
 
 def add():
     name = input("Account Name: ")
     password = input("Password: ")
 
     with open("passwords.txt", "a") as f:
-        f.write(name + " | " + password + "\n")
+        f.write(name + " | " + fer.encrypt(password.encode()).decode() + "\n")
     
 
 while True:
